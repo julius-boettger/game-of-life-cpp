f176d4a7aa60a517445795de39cc63e040fbecb5
julius-boettger <julius.btg@proton.me>
2024-11-26 21:38:32 +0100

    refactor: use list initialization

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 #include <ftxui/component/screen_interactive.hpp>
 
 int main() {
-    auto screen = ui::ScreenInteractive::Fullscreen();
+    auto screen { ui::ScreenInteractive::Fullscreen() };
 
-    auto renderer = ui::Renderer([&] {
+    auto renderer { ui::Renderer([&] {
         return ui::center(ui::text(std::format("{} x {}", screen.dimx(), screen.dimy())));
-    });
+    }) };
 
     screen.SetCursor(ui::Screen::Cursor {
         .shape = ui::Screen::Cursor::Shape::Hidden

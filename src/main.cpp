f30562b5735fdc3093e95c370c525d5f616592f4
julius-boettger <julius.btg@proton.me>
2024-11-26 21:08:26 +0100

    feat: setup fullscreen test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+#include "globals.hpp"
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 
 int main() {
+    auto screen = ui::ScreenInteractive::Fullscreen();
+    auto renderer = ui::Renderer([] {
+        return ui::center(ui::text("Test"));
+    });
+    screen.Loop(renderer);
+ 
     return 0;
 }

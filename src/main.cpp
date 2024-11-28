7b84eeb475656bf944baf1700a747f8c8ffff69d
julius-boettger <julius.btg@proton.me>
2024-11-28 20:03:17 +0100

    feat: implement controlled exiting

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,45 @@
 
 #include <ftxui/component/screen_interactive.hpp>
 
+bool running { true };
+std::thread update_thread;
+auto screen { ui::ScreenInteractive::Fullscreen() };
+
+void exit() {
+    running = false;
+    if (update_thread.joinable()) {
+        update_thread.join();
+    }
+
+    screen.ExitLoopClosure()();
+}
+
 int main() {
     constexpr std::chrono::milliseconds FRAME_TIME { 500 };
     const std::string emptyField { " " };
 
-    auto screen { ui::ScreenInteractive::Fullscreen() };
     screen.SetCursor(ui::Screen::Cursor {
         .shape = ui::Screen::Cursor::Shape::Hidden
     });
 
     Grid grid { ui::Dimension::Full(), emptyField };
 
-    std::thread update_thread([&] {
-        while (true) {
+    update_thread = std::thread([&] {
+        while (running) {
             std::this_thread::sleep_for(FRAME_TIME);
             grid.update();
             screen.RequestAnimationFrame();
         }
     });
 
-    screen.Loop(grid.getComponent());
+    auto event_handler = ui::CatchEvent(grid.getComponent(), [&](ui::Event event) {
+        if (event == ui::Event::Character("q")) {
+            exit();
+            return true; // event handled
+        }
+        return false; // event not handled
+    });
+
+    screen.Loop(event_handler);
     return 0;
 }

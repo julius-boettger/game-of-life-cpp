707a032f9afe6cce7685f607b09b875422fba5fa
julius-boettger <julius.btg@proton.me>
2024-11-29 22:02:39 +0100

    feat: improve termination handling

--- src/main.cpp
+++ src/main.cpp
@@ -5,45 +5,41 @@
 
 #include <ftxui/component/screen_interactive.hpp>
 
-std::thread update_thread;
-std::atomic<bool> running { true };
-auto screen { ui::ScreenInteractive::Fullscreen() };
-
-void exit() {
-    running = false;
-    if (update_thread.joinable()) {
-        update_thread.join();
-    }
-
-    screen.ExitLoopClosure()();
-}
-
 int main() {
     constexpr std::chrono::milliseconds FRAME_TIME { 500 };
     const std::string emptyField { " " };
 
+    auto screen { ui::ScreenInteractive::Fullscreen() };
     screen.SetCursor(ui::Screen::Cursor {
         .shape = ui::Screen::Cursor::Shape::Hidden
     });
 
     Grid grid { ui::Dimension::Full(), emptyField };
 
-    update_thread = std::thread([&] {
-        while (running) {
+    std::atomic<bool> update_thread_running { true };
+    std::thread update_thread { ([&] {
+        while (update_thread_running) {
             std::this_thread::sleep_for(FRAME_TIME);
             grid.update();
             screen.RequestAnimationFrame();
         }
-    });
+    }) };
 
     auto event_handler = ui::CatchEvent(grid.getComponent(), [&](ui::Event event) {
         if (event == ui::Event::Character("q")) {
-            exit();
+            screen.ExitLoopClosure()();
             return true; // event handled
         }
         return false; // event not handled
     });
 
+    // start main loop which blocks the main thread
+    // terminates on screen.ExitLoopClosure()() or SIGINT
     screen.Loop(event_handler);
+    // main loop has ended, clean up!
+
+    update_thread_running = false;
+    update_thread.join();
+
     return 0;
 }
